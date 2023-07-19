bda75b4e3bc89ad1845a22c4601744fed6a1f1f9
ojinseog <onlycan17@gmail.com>
2023-07-19 11:35:57 +0900

    [feature] : 헤더수정

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,83 +1,116 @@
-import '../../styles/css/header.module.css';
-import Link from "next/link";
+import {useState, useEffect, MouseEvent, FocusEvent} from "react";
+import Link from 'next/link';
+
+type SubmenuList = {
+    seq: number;
+    title: string;
+    link: string;
+}
 
 const Header: React.FC = () => {
+
+
+    const [menuActive, setMenuActive] = useState<boolean>(false);
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+    const [submenuHeight, setSubmenuHeight] = useState<number | null>(null);
+    const [subMenuList, setSubMenuList] = useState<SubmenuList[]>([]);
+    const [submenuListOne, setSubmenuListOne] = useState<SubmenuList[]>([]);
+    const [submenuListTwo, setSubmenuListTwo] = useState<SubmenuList[]>([]);
+    const [submenuListThree, setSubmenuListThree] = useState<SubmenuList[]>([]);
+    const [submenuListFour, setSubmenuListFour] = useState<SubmenuList[]>([]);
+    const [submenuListFive, setSubmenuListFive] = useState<SubmenuList[]>([]);
+    const [submenuListSix, setSubmenuListSix] = useState<SubmenuList[]>([]);
+
+    useEffect(() => {
+        const cmsGnb = document.getElementById("cms-gnb");
+        if (cmsGnb) {
+            const ulElement = cmsGnb.querySelector("ul");
+            if (ulElement) {
+                setSubmenuHeight(ulElement.offsetHeight - 105);
+            }
+        }
+        setSubmenuListOne([
+            {seq: 1, title: '짝꿍찾기', link: '/views/matching',},
+            {seq: 2, title: '진행현황', link: '#',},
+            {seq: 3, title: '-전체현황', link: '/views/matching',},
+            {seq: 4, title: '-현재현황', link: '/views/matching',},
+        ]);
+        setSubmenuListTwo([
+            {seq: 1, title: '채팅신청', link: '/views/matching',},
+            {seq: 2, title: '화상신청', link: '/views/matching',},
+            {seq: 3, title: '만남신청', link: '/views/matching',},
+            {seq: 4, title: '면담신청', link: '/views/matching',},
+        ]);
+        setSubmenuListThree([
+            {seq: 1, title: '간증', link: '/views/matching',},
+            {seq: 2, title: '사전교육', link: '/views/matching',},
+            {seq: 3, title: '결혼예배학교', link: '/views/matching',},
+            {seq: 4, title: '생방송', link: '/views/matching',},
+        ]);
+        setSubmenuListFour([
+            {seq: 1, title: '예식상담', link: '/views/matching',},
+        ]);
+        setSubmenuListFive([
+            {seq: 1, title: '공지', link: '/views/matching',},
+            {seq: 2, title: '자유게시판', link: '/views/matching',},
+            {seq: 3, title: '질문과답변', link: '/views/matching',},
+        ]);
+        setSubmenuListSix([
+            {seq: 1, title: '기본정보', link: '/views/matching',},
+            {seq: 2, title: '개인프로필', link: '/views/matching',},
+            {seq: 3, title: '매칭이력', link: '/views/matching',},
+            {seq: 4, title: '서약서결과', link: '/views/matching',},
+            {seq: 5, title: '질문현황', link: '/views/matching',},
+        ]);
+    }, []);
+
+    const handleMouseOver = (index: number) => (_event: MouseEvent) => {
+        setMenuActive(true);
+        setActiveIndex(index);
+    }
+
+    const handleMouseLeave = () => (_event: MouseEvent) => {
+        setMenuActive(false);
+        setActiveIndex(null);
+    }
+
+    const handleFocus = (index: number) => (_event: FocusEvent) => {
+        setMenuActive(true);
+        setActiveIndex(index);
+    }
+
+    const submenuLists = [submenuListOne, submenuListTwo, submenuListThree, submenuListFour, submenuListFive, submenuListSix];
     return (
-        <div id="header" className="">
+        <div id="header" className={menuActive ? 'open' : ''}>
             <div className="bottom">
                 <div className="inner">
-                    <h1 className="logo">
-                        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-                        <Link href="/views/main">logo </Link>
-                    </h1>
                     <div id="cms-gnb">
                         <ul>
-                            <li className="menu1">
-                                <Link href="" id="menu-1" title="짝궁찾기">짝궁찾기</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-9" title="짝궁찾기">짝궁찾기</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-10" title="진행현황">진행현황</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-11" title="전체현황">-전체현황</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-11" title="현재현황">-현재현황</Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="menu2">
-                                <Link href="#" id="menu-2" title="메칭관리">매칭관리</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-14" title="채팅신청">채팅신청</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-14" title="화상신청">화상신청</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-14" title="만남신청">만남신청</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-14" title="면담신청">면담신청</Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="menu3">
-                                <Link href="" id="menu-3" title="결혼예비학교">결혼예비학교</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="간증">간증</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="사전교육">사전교육</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="결혼예비학교">결혼예비학교</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="생방송">생방송</Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="menu4">
-                                <Link href="" id="menu-4" title="상담실">상담실</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="예식상담">예식상담</Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="menu5">
-                                <Link href="" id="menu-5" title="공지사항">공지사항</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="공지">공지</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="자유게시판">자유게시판</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="질문과답변">질문과답변</Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="menu6">
-                                <Link href="" id="menu-6" title="마이페이지">마이페이지</Link>
-                                <ul>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="기본정보">기본정보</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="개인프로필">개인프로필</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="매칭이력">매칭이력</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="서약서결과">서약서결과</Link></li>
-                                    <li className=" font_reset"><Link href="" id="menu-20" title="질문현황">질문현황</Link></li>
-                                </ul>
-                            </li>
+                            {['짝궁찾기', '매칭관리', '결혼예배학교', '상담실', '공지사항', '마이페이지'].map((title, index) => (
+                                // eslint-disable-next-line react/jsx-key
+                                <li
+                                    className={`menu${index + 1} ${activeIndex === index ? 'on' : ''}`}
+                                    onMouseOver={handleMouseOver(index)}
+                                    onMouseLeave={handleMouseLeave()}
+                                >
+                                    <Link href="#" onFocus={handleFocus(index)}>
+                                        {title}
+                                    </Link>
+                                    <ul style={{height: submenuHeight ? `${submenuHeight}px` : ''}}>
+                                        {submenuLists[index].map((subMenu, idx) => (
+                                            <li key={idx} className="font_reset">
+                                                <Link id={`menu-${index}-${idx}`} href={`${subMenu.link}`}>{subMenu.title}</Link>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            ))}
                         </ul>
-                    </div>
-                    <div id="right_menu">
-                        <Link href={"/"}>Login</Link> <Link href={"/views/sign-up"}>sign up </Link>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
 
 export default Header;
