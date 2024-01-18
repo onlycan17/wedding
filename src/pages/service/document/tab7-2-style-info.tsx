7a1577d507dddc5cdf3932b83ba91e32cc7ea27f
Oh Jin-Seok <37200377+onlycan17@users.noreply.github.com>
2024-01-18 21:13:09 +0900

    저장

--- src/pages/service/document/tab7-2-style-info.tsx
+++ src/pages/service/document/tab7-2-style-info.tsx
@@ -103,6 +103,137 @@ const Tab72StyleInfo: NextPage<ActivedTab> = ({ activedDetailTab, setActivedDeta
           원하는 상대스타일을 모두 선택하세요 (복수선택가능)
         </h3>
       </div>
+      <div className="ml-[88px] mt-[60px] flex w-full">
+        <div className="text-[color:var(--900,#232527)] text-base not-italic font-bold leading-[150%] tracking-[0.16px]">
+          반려동물 유무
+        </div>
+        <div className=" flex flex-col items-center justify-center shrink-0 w-6 h-6">
+          <div
+            className="text-[#e82121] text-center flex items-center justify-center w-full h-full pt-[3px]"
+            style={{ font: "400 13px/14.95px 'Noto Sans', sans-serif" }}
+          >
+            *
+          </div>
+        </div>
+        <div className="ml-[38px]"></div>
+        <div className="flex">
+          <span className="bg-chk">
+            <input type="radio" name="petYn" id="petN" />
+            <label htmlFor="petN">무</label>
+          </span>
+          <span className="bg-chk">
+            <input type="radio" name="petYn" id="petY" />
+            <label htmlFor="petY">유</label>
+          </span>
+          <div className="mt-[-15px]">
+            <input
+              type="text"
+              className="flex w-[360px] h-[60px] items-center gap-6 shrink-0 border border-[color:var(--400,#BFBFBF)] px-2.5 py-0 rounded-[5px] border-solid"
+              placeholder="반려동물 종류 입력"
+            />
+          </div>
+        </div>
+        <div className="mb-[60px]"></div>
+      </div>
+      <div className="flex ml-[88px] ">
+        <div className="flex flex-col text-[color:var(--900,#232527)] text-base not-italic font-bold leading-[150%] tracking-[0.16px]">
+          <div className="mt-[52px] flex flex-row">
+            <p>상대</p>
+            <p className="ml-[13px] mt-[10px] text-[#E82121] text-center text-[13px] not-italic font-normal leading-[14.95px] tracking-[-0.26px] ">
+              *
+            </p>
+          </div>
+          <p>취미활동</p>
+        </div>
+        <div className="ml-[18px] mt-[52px] grid grid-cols-5 gap-[30px]">
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">게임 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">농구,배구,축구,야구 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">배드민턴 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">테니스 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">골프 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">승마 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">볼링 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">스키(보드) </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">당구 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">라이딩 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">필라테스 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">헬스/요가 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">수영(수상레저) </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">공연/영화/전시 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">여행 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">DIY </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">악기(밴드) </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">등산 </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">마라톤(달리기) </label>
+          </span>
+          <span className="bg-chk-square">
+            <input type="checkbox" id="proactive" name="proactive" />
+            <label htmlFor="proactive">요리/쿠킹 </label>
+          </span>
+        </div>
+      </div>
+      <div className="mt-[30px] ml-[170px] flex">
+        <img src="/alram.png" alt="" width={24} height={24} />
+        <h3 className="ml-[10px] text-[color:var(--600,#777)] text-sm not-italic font-normal leading-6">
+          짝꿍과 나중에 같이 하고픈 취미활동 3가지만 적어주세요
+        </h3>
+      </div>
     </div>
   );
 };
